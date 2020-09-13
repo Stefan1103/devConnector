3da71ffd7fff801c311327e58c853cc9f5243584
Stefan1103 <stefan.stevanovski11@gmail.com>
2020-09-13 16:02:48 +0200

    added authentication and Login with jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,14 +45,14 @@ router.post(
 				password
 			});
 			//Encrypt password
-			const salt = await bcrypt.genSalt(10);
-			user.password = await bcrypt.hash(password, salt);
+			const salt = bcrypt.genSaltSync(10);
+			user.password = bcrypt.hashSync(password, salt);
 			await user.save(); // zacuvaj go userot u bazata
 
 			//Return jsonwebtoken
 			const payload = {
 				user: {
-					id: user.id
+					id: user._id
 				}
 			};
 			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
